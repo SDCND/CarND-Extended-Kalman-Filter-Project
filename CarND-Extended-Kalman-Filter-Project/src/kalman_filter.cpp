19622a03e76beaf5f0c9701c0f76dfb793556574
SDCND <mario_capin@hotmail.com>
2017-04-27 12:00:42 +0200

    First commit

---
+++ CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -0,0 +1,97 @@
+#include "kalman_filter.h"
+#include "tools.h"
+#include <iostream>
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,  MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) 
+{
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() 
+{
+  /**
+  TODO:
+    * predict the state
+  */
+
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) 
+{
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+
+  // prediction error
+  VectorXd y = z - H_ * x_; 
+
+  // perform rest of update
+  UpdateOps(y);
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) 
+{
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+
+  // convert current state into polar coordinate
+  float ro = sqrt(x_[0] * x_[0] + x_[1] * x_[1]);
+  float phi = 0.0; // make phi 0.0 if too small
+  
+  if (fabs(x_[0]) > 0.001) 
+  {
+    phi = atan2(x_[1], x_[0]);
+  }
+  
+  float ro_dot = 0.0; // make ro_dot 0.0 if too small
+  
+  if (fabs(ro) > 0.001) 
+  {
+    ro_dot = (x_[0] * x_[2] + x_[1] * x_[3]) / ro;
+  }
+
+  VectorXd hx(3);
+  hx << ro, phi, ro_dot;
+  
+  // prediction error
+  VectorXd y = z - hx;
+  
+  // perform rest of update
+  UpdateOps(y);
+}
+
+void KalmanFilter::UpdateOps(const VectorXd &y) 
+{
+  /*
+  perform rest of kalman filter update
+  */
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si; // kalman gain
+
+  // new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
---
+++ CarND-Extended-Kalman-Filter-Project/src/tools.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) 
+{
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // Make sure estimation vector size is not zero and,
+  // the estimation vector size is equal to the ground truth vector size
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0)
+  {
+    std::cout << "Invalid estimation or ground_truth data" << std::endl;
+
+    return rmse;
+  }
+	
+  //accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i)
+  {
+	VectorXd residual = estimations[i] - ground_truth[i];
+
+	//coefficient-wise multiplication
+	residual = residual.array()*residual.array();
+	rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse/estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) 
+{
+  /**
+  TODO:
+    * Calculate a Jacobian here.
+  */
+  
+  MatrixXd Hj(3, 4);
+	
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  //pre-compute a set of terms to avoid repeated calculation
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+
+  //check division by zero
+  if (fabs(c1) < 0.00001)
+  {
+    VectorXd x_temp(4);
+
+    // in case of division by zero, set state to small float value
+    x_temp << 0.001, 0.01, 0.001, 0.001;
+    std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+    
+	return CalculateJacobian(x_temp);
+  }
+
+  //compute the Jacobian matrix
+  Hj << (px / c2), (py / c2), 0, 0,	-(py / c1), (px / c1), 0, 0, py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
+	
+  return Hj;
+}
